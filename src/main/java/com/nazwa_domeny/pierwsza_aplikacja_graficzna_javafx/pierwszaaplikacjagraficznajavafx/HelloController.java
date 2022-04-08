27c7b84f77633939535d3c8f94fde5e4df85b889
Justyna Opolska <justopol@wp.pl>
2022-04-08 12:45:06 +0200

    java fx1

---
+++ src/main/java/com/nazwa_domeny/pierwsza_aplikacja_graficzna_javafx/pierwszaaplikacjagraficznajavafx/HelloController.java
@@ -0,0 +1,14 @@
+package com.nazwa_domeny.pierwsza_aplikacja_graficzna_javafx.pierwszaaplikacjagraficznajavafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.nazwa_domeny.pierwsza_aplikacja_graficzna_javafx.pierwszaaplikacjagraficznajavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.nazwa_domeny.pierwsza_aplikacja_graficzna_javafx.pierwszaaplikacjagraficznajavafx to javafx.fxml;
+    exports com.nazwa_domeny.pierwsza_aplikacja_graficzna_javafx.pierwszaaplikacjagraficznajavafx;
+}
